ab54f1f078afa04aa7b68f482b7d067310272844
MaxLacasseG <max.lacasse.g@gmail.com>
2018-09-26 17:18:16 -0400

    installation

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
+const logger = require("tracer").colorConsole();
+
+//require routes
 
 const mongoose = require("mongoose");
-d;
+//connect mongoose
+
+//Add passport
+
+//use middlewares
+
+// use routes
+app.listen(port, err => {
+    logger.trace(`listening on port ${port}`);
+});
